1c8de9758d86fdb0448d1f9b87aa63dccfa343ca
abdallahahmed18 <abdallaahmedabdash@gmail.com>
2024-05-27 16:31:59 +0300

    Adding favicon to home.php

--- home.php
+++ home.php
@@ -21,6 +21,7 @@ include 'components/wishlist_cart.php';
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>home</title>
+   <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
    
